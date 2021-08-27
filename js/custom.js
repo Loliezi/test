4ca2b2643f3db309cc5dc15f23df61cf15df9fe0
Loliezi <52872248+Loliezi@users.noreply.github.com>
2021-08-27 14:29:54 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -169,11 +169,7 @@ jQuery(function($){
 	/*  8. PRELOADER 
 	/* ----------------------------------------------------------- */ 
 
-	jQuery(window).on('load', function() { // makes sure the whole site is loaded
-      $('.progress').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(100).css({'overflow':'visible'});
-    })
+	jQuery(window).load(function(){$('#preloader').fadeOut()})
 	  
 	/* ----------------------------------------------------------- */
 	/* 9. CALL TO ABOUT
