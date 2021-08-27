52ce0ffa5d2cff608ba66d43c4bf039a10d69e6c
Loliezi <52872248+Loliezi@users.noreply.github.com>
2021-08-27 14:19:00 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -36,7 +36,7 @@ jQuery(function($){
 	/*  2. Experience SLider(Owl Carousel)
 	/* ----------------------------------------------------------- */
 
-	var owl = $(".owl-carousel"); 
+	var owl = $(".doccat-carousel"); 
     owl.owlCarousel({
         items: 4,
         /* transitionStyle : "fade", */
